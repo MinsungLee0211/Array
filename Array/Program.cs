68d959e893383c9e2c3108f384165ea1fc124037
chocomin0211 <chocomin0211@naver.com>
2018-10-05 20:03:58 +0900

    첫 커밋

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,7 +6,17 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intarr = { 0, 1, 2, 3 };
+            /*
+             * c언어에서는
+             * int* inarr의 형태로 처리하여 위치 포인터와 같이 함
+             */
+
+            char[] chararr = { '안', '녕', '하', '귀', '찮', '아' };
+            string mystring = "안녕하귀찮아";
+
+            Console.WriteLine(chararr);
+            Console.ReadKey();
         }
     }
 }
